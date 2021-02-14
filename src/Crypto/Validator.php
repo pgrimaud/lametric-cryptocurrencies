a1577db447aa6e798c6f8ea943e97f57fcb0a13f
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-02-14 18:10:16 +0100

    Fix currency as lowercase

--- src/Crypto/Validator.php
+++ src/Crypto/Validator.php
@@ -51,7 +51,7 @@ class Validator
         $this->data['change']   = isset($this->parameters['change']) && strtolower($this->parameters['change']) === 'yes';
         $this->data['names']    = !isset($this->parameters['show_label']) || strtolower($this->parameters['show_label']) === 'yes';
         $this->data['position'] = $this->parameters['position'] ?? Response::POSITION_AFTER; // after is default position
-        $this->data['currency'] = $this->parameters['currency'] ?? 'USD'; // USD is default
+        $this->data['currency'] = strtoupper($this->parameters['currency']) ?? 'USD'; // USD is default
     }
 
     /**
