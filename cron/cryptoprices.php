53bbd96002992891c70034c580a24f206c6f11d8
Pierre Grimaud <grimaud.pierre@gmail.com>
2023-03-17 14:53:08 +0100

    Improve crypto fetch

--- cron/cryptoprices.php
+++ cron/cryptoprices.php
@@ -5,15 +5,23 @@ require __DIR__ . '/../vendor/autoload.php';
 $parameters = require_once __DIR__ . '/../config/parameters.php';
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
 use Predis\Client as PredisClient;
 
 $http = new GuzzleClient();
 
 $allCurrencies = [];
 
+$redisKey = 'lametric:cryptocurrencies';
+
+$predis = new PredisClient();
+$redisObject = $predis->get($redisKey);
+
+$currencies = $redisObject ? json_decode($redisObject, true) : [];
+
 for ($i = 1; $i <= 25; $i++) {
 
-    echo 'Page ' . $i . PHP_EOL;
+    echo 'Page ' . $i;
 
     if (isset($parameters['proxies']) && count($parameters['proxies']) > 0) {
         $totalOfProxies = count($parameters['proxies']);
@@ -25,36 +33,37 @@ for ($i = 1; $i <= 25; $i++) {
         $headers = [];
     }
 
-    $response = $http->request(
-        'GET',
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&per_page=250&page=' . $i,
-        $headers
-    );
+    try {
+        $response = $http->request(
+            'GET',
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&per_page=250&page=' . $i,
+            $headers
+        );
 
-    $currencies = json_decode(strval($response->getBody()), true);
+        $currencies = json_decode(strval($response->getBody()), true);
 
-    foreach ($currencies as $currency) {
-        $symbol = strtoupper($currency['symbol']);
-        $price = $currency['current_price'];
-        $percent = $currency['price_change_percentage_24h'];
+        foreach ($currencies as $currency) {
+            $symbol = strtoupper($currency['symbol']);
+            $price = $currency['current_price'];
+            $percent = $currency['price_change_percentage_24h'];
 
-        if(!isset($allCurrencies[$symbol])) {
-            $allCurrencies[$symbol] = [
-                'price' => $price,
-                'change' => $percent
-            ];
+            if (!isset($allCurrencies[$symbol])) {
+                $allCurrencies[$symbol] = [
+                    'price' => $price,
+                    'change' => $percent
+                ];
+            }
         }
+
+    } catch (ClientException $e) {
+        echo ' : ' . $e->getCode();
     }
+
+    echo PHP_EOL;
 
     // avoid 429 errors
     sleep(1);
 }
-
-$redisKey = 'lametric:cryptocurrencies';
-
-$predis = new PredisClient();
-
-$redisObject = $predis->get($redisKey);
 
 $predis->set($redisKey, json_encode($allCurrencies));
 
